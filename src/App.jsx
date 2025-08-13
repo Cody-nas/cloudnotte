412a2a61394e9e729c3efc259f9136980a56239a
Cody-nas <174928953+Cody-nas@users.noreply.github.com>
2025-08-13 11:57:29 +0100

    feat: add authentication forms and GraphQL setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useState } from "react";
 import React from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1 class="text-3xl font-bold text-emerald-500 underline">
---
+++ src/graphql/mutation.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const mutation = () => {
+  return <div>mutation</div>;
+};
+
+export default mutation;
